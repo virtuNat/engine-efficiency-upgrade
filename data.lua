474c447c9d3aeb29c1097f99050ca3a762f6c4c4
Janine Dirichlet <19920458+virtuNat@users.noreply.github.com>
2026-06-05 09:48:46 +0800

    Fix Cerys compat

--- data.lua
+++ data.lua
@@ -1,5 +1,11 @@
--- Recipes to add.
+-- Recipes to add/modify.
 if mods["space-age"] or mods["um-standalone-foundry"] then
+	-- local engine_recipe = data.raw.recipe["engine-unit"]
+	-- if not engine_recipe.additional_categories then
+	-- 	engine_recipe.additional_categories = {}
+	-- end
+	-- table.insert(engine_recipe.additional_categories, "metallurgy")
+
 	local engine_recipe = table.deepcopy(data.raw.recipe["engine-unit"])
 	engine_recipe.name = "engine-pressing"
 	engine_recipe.category = "metallurgy"
@@ -9,17 +15,16 @@ if mods["space-age"] or mods["um-standalone-foundry"] then
 end
 
 if mods["space-age"] or mods["um-standalone-electromagnetic-plant"] then
+	-- local electric_engine_recipe = data.raw.recipe["electric-engine-unit"]
+	-- if not electric_engine_recipe.additional_categories then
+	-- 	electric_engine_recipe.additional_categories = {}
+	-- end
+	-- table.insert(electric_engine_recipe.additional_categories, "electromagnetics")
+
 	local electric_engine_recipe = table.deepcopy(data.raw.recipe["electric-engine-unit"])
 	electric_engine_recipe.name = "electric-engine-magnetizing"
 	electric_engine_recipe.category = "electromagnetics"
 	electric_engine_recipe.enabled = true
-
-	if mods["Cerys-Moon-of-Fulgora"] then
-		table.insert(electric_engine_recipe.surface_conditions, {
-			property = "cerys-ambient-radiation",
-			min = 400,
-		})
-	end
 
 	data:extend {electric_engine_recipe}
 end
