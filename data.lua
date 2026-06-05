1ace07348474eb5d0af5f6ab96f5e433a5181a03
Janine Dirichlet <19920458+virtuNat@users.noreply.github.com>
2026-06-05 11:08:23 +0800

    Remove old unnecessary code

--- data.lua
+++ data.lua
@@ -5,13 +5,6 @@ if mods["space-age"] or mods["um-standalone-foundry"] then
 		engine_recipe.additional_categories = {}
 	end
 	table.insert(engine_recipe.additional_categories, "metallurgy")
-
-	-- local engine_recipe = table.deepcopy(data.raw.recipe["engine-unit"])
-	-- engine_recipe.name = "engine-pressing"
-	-- engine_recipe.category = "metallurgy"
-	-- engine_recipe.enabled = true
-
-	-- data:extend {engine_recipe}
 end
 
 if mods["space-age"] or mods["um-standalone-electromagnetic-plant"] then
@@ -20,11 +13,4 @@ if mods["space-age"] or mods["um-standalone-electromagnetic-plant"] then
 		electric_engine_recipe.additional_categories = {}
 	end
 	table.insert(electric_engine_recipe.additional_categories, "electromagnetics")
-
-	-- local electric_engine_recipe = table.deepcopy(data.raw.recipe["electric-engine-unit"])
-	-- electric_engine_recipe.name = "electric-engine-magnetizing"
-	-- electric_engine_recipe.category = "electromagnetics"
-	-- electric_engine_recipe.enabled = true
-
-	-- data:extend {electric_engine_recipe}
 end
