1a211e30eeae06e0d3e54f0da1b2b8e72cd7d3cd
Janine Dirichlet <19920458+virtuNat@users.noreply.github.com>
2026-08-16 12:32:54 +0800

    Update to Factorio 2.1

--- data.lua
+++ data.lua
@@ -1,16 +1,8 @@
 -- Recipes to modify.
 if mods["space-age"] or mods["um-standalone-foundry"] then
-	local engine_recipe = data.raw.recipe["engine-unit"]
-	if not engine_recipe.additional_categories then
-		engine_recipe.additional_categories = {}
-	end
-	table.insert(engine_recipe.additional_categories, "metallurgy")
+	table.insert(data.raw.recipe["engine-unit"].categories, "metallurgy")
 end
 
 if mods["space-age"] or mods["um-standalone-electromagnetic-plant"] then
-	local electric_engine_recipe = data.raw.recipe["electric-engine-unit"]
-	if not electric_engine_recipe.additional_categories then
-		electric_engine_recipe.additional_categories = {}
-	end
-	table.insert(electric_engine_recipe.additional_categories, "electromagnetics")
+	table.insert(data.raw.recipe["electric-engine-unit"].categories, "electromagnetics")
 end
